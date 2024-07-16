f23fce7a2eb518c991d3fa795a5570aeb44624ad
Bobby <92181277+bobby-isom@users.noreply.github.com>
2024-07-16 21:01:57 +0100

    Reddit Request

---
+++ Microsoft Exchange Online/Delete Emails Based on Body Content.ps1
@@ -0,0 +1,20 @@
+﻿#Install Modules
+Install-Module -Name ExchangeOnlineManagement -Scope AllUsers
+
+#Connect to Exchange 365
+Connect-IPPSSession -UserPrincipalName EmailAddress
+
+#Search with Body
+$Search=New-ComplianceSearch -Name $SearchInfo -ExchangeLocation All -ContentMatchQuery '(Received:03/10/2020..03/10/2020) AND (Body:"Gavi Sandhu shared the folder "Lifetime Wealth Management Ltd" with you."'
+
+#Enter the same name as the previous step
+$SearchInfo = Read-Host "Enter Search Name"
+
+#Starts the Compliance Search
+Start-ComplianceSearch -Identity $Search.Identity
+
+#Deletes the email from mailboxes
+New-ComplianceSearchAction -SearchName $SearchInfo -Purge -PurgeType HardDelete
+
+#Disconnect from Exchange 365
+Disconnect-ExchangeOnline
---
+++ Microsoft Exchange Online/Delete Emails Based on Subject.ps1
@@ -0,0 +1,20 @@
+﻿#Install Modules
+Install-Module -Name ExchangeOnlineManagement -Scope AllUsers
+
+#Connect to Exchange 365
+Connect-IPPSSession -UserPrincipalName EmailAddress
+
+#Search with Subject in MM/DD/YYYY format
+$Search=New-ComplianceSearch -Name "2022.06.10 Remove Phishing Email - Memorandum of sale" -ExchangeLocation All -ContentMatchQuery '(Received:06/10/2022..06/10/2022) AND (Subject:"Memorandum of sale  06/10/2022 06:57:37 am")'
+
+#Enter the same name as the previous step
+$SearchInfo = Read-Host "Enter Search Name"
+
+#Starts the Compliance Search
+Start-ComplianceSearch -Identity $Search.Identity
+
+#Deletes the email from mailboxes
+New-ComplianceSearchAction -SearchName $SearchInfo -Purge -PurgeType HardDelete
+
+#Disconnect from Exchange 365
+Disconnect-ExchangeOnline
